c01a62b75a50549680b4bf1360ce4f381bd60e68
“Nacho <nacho.guarino@live.com.ar>
2023-02-16 15:09:35 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+//Simulador de Prestamos Ignacio Guarino
+
+function calcularPrestamo(monto, tasaInteres, plazo) {
+
+    let tasaMensual = tasaInteres / 12 / 100
+
+    let cuota = monto * tasaMensual / (1 - Math.pow(1 + tasaMensual, -plazo))
+
+    let = cuota.toFixed(2)
+
+    return cuota
+}
+
+let monto = prompt("Ingrese el monto del préstamo: ")
+let tasaInteres = prompt("Ingrese la tasa de interés anual: ")
+let plazo = prompt("Ingrese el plazo en meses: ")
+
+monto = parseFloat(monto)
+tasaInteres = parseFloat(tasaInteres)
+plazo = parseInt(plazo)
+
+if (isNaN(monto) || isNaN(tasaInteres) || isNaN(plazo)) {
+    alert("Por favor ingrese valores numéricos.")
+}
+else {
+    let cuotaMensual = calcularPrestamo(monto, tasaInteres, plazo)
+
+    alert("La cuota mensual que debera abonar es de: $"+ cuotaMensual)
+}
